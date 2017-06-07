4d1daabe40b140f71fa05ff0cf17842221bad337
Dominic-Pace <dpace@nhpace.net>
2017-06-07 00:16:29 -0400

    Updates to the authentication endpoint. Now returns user data upon login. Also references a .env file now.

--- src/middleware/auth/index.js
+++ src/middleware/auth/index.js
@@ -1,25 +1,34 @@
+require ('dotenv').config();
 import jwt from 'jsonwebtoken';
 import expressJwt from 'express-jwt';
 
 const TOKEN_TIME = 60 * 60 * 24 * 365; //1 Year
-const SECRET = "S42fSp1qe250M9vWVsx4h12uB";
 
-let authenticate = expressJwt({ secret: SECRET });
+let authenticate = expressJwt({ secret: process.env.SECRET });
 let generateAccessToken = (req,res, next) => {
   req.token = req.token || {};
   req.token = jwt.sign({
     id: req.user.id,
-  }, SECRET, {
+  }, process.env.SECRET, {
     expiresIn: TOKEN_TIME // 1 Year
   });
   next();
 };
 
 let respond = (req, res) => {
+  let returnedUser = req.user.toObject();
+
+  delete returnedUser.salt
+  delete returnedUser.hash
+  delete returnedUser.__v
+
   res.status(200).json({
     status: 'Success!',
     message: 'User authenticated successfully.',
-    token: req.token
+    data:{
+      token: req.token,
+      user: returnedUser
+    }
   });
 };
 
